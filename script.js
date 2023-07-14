affd9d91b756f9d07f4348dad64113a7e49728ca
salsabeel noor <salsabeelnoor@gmail.com>
2023-07-14 18:34:05 +0600

    async-await implemented

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const spinner = document.getElementById('spinner');
 const clickEventBox = document.getElementById('click-event-container');
 
-document.getElementById('btn').addEventListener('click', function(){
+async function delay (time){
+    return new Promise (resolve => {
+        setTimeout(resolve, time)
+    })
+}
+document.getElementById('btn').addEventListener('click', async function(){
     spinner.style.display = 'block';
-    setTimeout(() => {
-        clickEventBox.style.display = 'flex'   
-        spinner.style.display = 'none'
-    }, 3000)
+    await delay(3000);
+    clickEventBox.style.display = 'flex'; 
+    spinner.style.display = 'none';
 })
+
 document.getElementById('btn-reset').addEventListener('click', function(){
     spinner.style.display = 'none';
     clickEventBox.style.display = 'none' 
