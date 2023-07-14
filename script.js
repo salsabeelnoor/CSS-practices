77bd018c69ad6a7909370ac947178ccadf700bf7
salsabeel noor <salsabeelnoor@gmail.com>
2023-07-14 17:21:33 +0600

    test2

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const spinner = document.getElementById('spinner');
 const clickEventBox = document.getElementById('click-event-container');
 
-
 document.getElementById('btn').addEventListener('click', function(){
     spinner.style.display = 'block';
     setTimeout(() => {
         clickEventBox.style.display = 'flex'   
         spinner.style.display = 'none'
     }, 3000)
+})
+document.getElementById('btn-reset').addEventListener('click', function(){
+    spinner.style.display = 'none';
+    clickEventBox.style.display = 'none' 
 })
   
